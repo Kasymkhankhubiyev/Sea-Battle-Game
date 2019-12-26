84b9a6252bb8b7726295cf0dc9a81b7ecd4a1e0d
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2019-12-26 09:01:06 +0700

    Create Graphics.h

---
+++ Graphics.h
@@ -0,0 +1,23 @@
+#ifndef _GRAPHICS_H_
+#define _GRAPHICS_H_
+#include <stdlib.h>
+#include <iostream>
+
+//#include "Game.h"
+
+
+class Field
+{
+	size_t m_arr[20][20];
+public:
+	void Draw_score();
+
+	void Draw_field_line(size_t arr[20][20]);
+	
+	void Draw_player_field(size_t arr[20][20]);
+};
+
+
+
+#endif // !_GRAPHICS_H_
+#pragma once
