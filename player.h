14bdfdda3dee1ac5192023f87a30bc14501a4e57
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2019-12-26 19:40:48 +0700

    Update player.h

--- player.h
+++ player.h
@@ -7,23 +7,38 @@
 #include <windows.h>
 #include <dos.h>
 #include <stdio.h>
+#include <ctime>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cstring>
 
-
-class Player
+class Player: public Field
 {
+	size_t my_ind;
 	size_t Score;
-	size_t my_field[10][10];
-	size_t battle_field[10][10];
+	char my_field[22][22];
+	char battle_field[22][22];
 	size_t boat;
 	size_t destroyer;
 	size_t cruiser;
 	size_t buttle_ship;
+	size_t shoot;
+	//std::map<size_t, pair <size_t, size_t>> shoot_story;
 public:
 	Player();
 
-	size_t make_shoot();
+	bool Compare1(size_t& x, size_t& y);
+
+	size_t Compare2(size_t& x, size_t& y);
+
+	void Make_shoot(size_t& x, size_t& y);
 
 	void Set_ship_type();
+
+	void ReDraw();
+
+	void Reset(size_t& x, size_t& y);
 
 	void Draw_player_field();
 
@@ -32,6 +47,9 @@ public:
 	size_t Get_ships();
 
 	void Draw_battle_field();
+
+	void Wait_func();
 };
+
 #endif // !_PLAYER_H_
 #pragma once
